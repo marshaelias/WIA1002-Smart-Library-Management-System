983d6a9d643f96527adfa9c86c5f5f122f19fb8f
alisharemy <icha.remy@gmail.com>
2026-05-13 09:20:26 +0800

    Defined Book class and inserted the logic for insert method in BookBST class

---
+++ src/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    int isbn;
+    String title;
+    String author;
+    Book left;
+    Book right;
+    
+    public Book(int isbn, String title, String author){
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.left = null;
+        this.right = null;
+    }
+}
---
+++ src/BookBST.java
@@ -0,0 +1,23 @@
+public class BookBST {
+    private Book root; // information hiding so it cant be accidentally deleted or overwritten
+    
+    public BookBST() {
+        this.root =  null;
+    }
+    
+    public void insert(int isbn, String title, String author) {
+        root = insertRecursive(root, isbn, title, author);
+    }
+    
+    private Book insertRecursive(Book current, int isbn, String title, String author) {
+        if (current == null) {
+            return new Book(isbn, title, author);
+        } else if (isbn < current.isbn) {
+            current.left = insertRecursive(current.left, isbn, title, author);
+        } else {
+            current.right = insertRecursive(current.right, isbn, title, author);
+        }
+        // smaller isbn go left, larger isbn go right
+        return current;
+    }
+}
