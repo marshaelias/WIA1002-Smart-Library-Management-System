7bd98330f5fbe1c9cf4cf9d674c166f3a07b855f
aisyahazaleeya <24001041@siswa.um.edu.my>
2026-05-20 00:50:28 +0800

    Filled the LibraryADT interface class and SmartLibrary class.

---
+++ src/LibraryADT.java
@@ -0,0 +1,7 @@
+
+public interface LibraryADT{
+    void addBook(int isbn, String title, String author);
+    void borrowBook(int isbn);
+    void viewLatestHistory();
+    void searchBook(int isbn);
+}
