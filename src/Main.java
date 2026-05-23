7b13f201cccc6f322efbfb7f39795f5bf1c4c35f
User <User@DESKTOP-33MDJ4J>
2026-05-23 16:07:45 +0800

    implemented SmartLibrary admin logic and Main class

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // Instantiate the SmartLibrary system
+        SmartLibrary librarySystem = new SmartLibrary();
+        
+        // Trigger the menu to start the program
+        librarySystem.runMenu();
+    }
+}
--- src/SmartLibrary.java
+++ src/SmartLibrary.java
@@ -1,29 +1,48 @@
 import java.util.Scanner;
 
-public class SmartLibrary implements LibraryADT{
+public class SmartLibrary implements LibraryADT {
     
-    //information hiding
+    // Information hiding
     private BookBST catalogue = new BookBST();
     private BorrowStack history = new BorrowStack();
     
     @Override
     public void addBook(int isbn, String title, String author){
-        
+        // Pass data to Alisha's BST
+        catalogue.insert(isbn, title, author);
+        System.out.println("Success: Book added to the catalogue.");
     }
     
     @Override
     public void searchBook(int isbn){
-        
+        // Use Auni's O(log n) search
+        Book b = catalogue.search(isbn);
+        if (b != null) {
+            System.out.println("Found -> Title: " + b.title + " | Author: " + b.author);
+        } else {
+            System.out.println("Not Found: No book matches that ISBN.");
+        }
     }
     
     @Override
     public void borrowBook(int isbn){
+        // 1. Verify if it exists first
+        Book b = catalogue.search(isbn);
         
+        if (b != null) {
+            // 2. If it exists, push to Marsha's stack
+            history.push(b);
+            System.out.println("Success: Borrowed '" + b.title + "'. Added to history.");
+        } else {
+            // 3. If it doesn't exist, handle the error gracefully
+            System.out.println("Error: Book not found in catalogue. Cannot borrow.");
+        }
     }
     
     @Override
     public void viewLatestHistory(){
-        
+        // Trigger Marsha's history display
+        history.show();
     }
     
     public void printMenu(){
